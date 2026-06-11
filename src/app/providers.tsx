1893e2028570670d500e94b8169aa7fe2b9aeef8
RohanDas26 <rohandass2626@gmail.com>
2026-06-11 09:38:20 +0530

    fix: remove SessionProvider crashing app hydration, use direct OAuth redirects

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,14 @@
 'use client';
-import { SessionProvider } from 'next-auth/react';
 import { AppProvider } from './AppContext';
 import { Navbar } from './components/Navbar';
 import { Toaster } from 'react-hot-toast';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <AppProvider>
-        <Navbar />
-        {children}
-        <Toaster position="top-right" />
-      </AppProvider>
-    </SessionProvider>
+    <AppProvider>
+      <Navbar />
+      {children}
+      <Toaster position="top-right" />
+    </AppProvider>
   );
 }
